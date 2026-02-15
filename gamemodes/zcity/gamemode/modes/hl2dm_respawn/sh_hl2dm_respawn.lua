ec0242ba09a24831358c80a4146534ec3153b3ae
Wade <82962983+Mpfwade@users.noreply.github.com>
2026-02-14 21:59:26 -0500

    bro

--- gamemodes/zcity/gamemode/modes/hl2dm_respawn/sh_hl2dm_respawn.lua
+++ gamemodes/zcity/gamemode/modes/hl2dm_respawn/sh_hl2dm_respawn.lua
@@ -11,14 +11,6 @@ zb = zb or {}
 zb = zb or {}
 zb.Points = zb.Points or {}
 
-zb.Points.HL2DM_SNIPERSPAWN = zb.Points.HL2DM_SNIPERSPAWN or {}
-zb.Points.HL2DM_SNIPERSPAWN.Color = Color(243,9,9)
-zb.Points.HL2DM_SNIPERSPAWN.Name = "HL2DM_SNIPERSPAWN"
-
-zb.Points.HL2DM_CROSSBOWSPAWN = zb.Points.HL2DM_CROSSBOWSPAWN or {}
-zb.Points.HL2DM_CROSSBOWSPAWN.Color = Color(243,9,9)
-zb.Points.HL2DM_CROSSBOWSPAWN.Name = "HL2DM_CROSSBOWSPAWN"
-
 -- Respawn configuration (shared so clients know the values)
 MODE.RespawnLives = 3
 MODE.RespawnDelay = 10
